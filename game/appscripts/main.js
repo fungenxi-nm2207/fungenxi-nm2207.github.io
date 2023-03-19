28f0d448109a2128ac1b405dc9783571c8a6ddfd
fungenxi-nm2207 <125212721+fungenxi-nm2207@users.noreply.github.com>
2023-03-19 20:31:42 +0800

    Add files via upload

--- game/appscripts/main.js
+++ game/appscripts/main.js
@@ -1,15 +1,38 @@
-let counter = 0;
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("button").onclick = count;
+// The start page and game pages
+const startPage = document.getElementById("start-page");
+const gamePage1 = document.getElementById("game-page-1");
+const gamePage2 = document.getElementById("game-page-2");
+const gamePage3 = document.getElementById("game-page-3");
+
+// Start game button
+const startbutton = document.getElementById("start-button");
+startbutton.addEventListener("click", function (){
+  startPage.classList.add("hidden");
+  gamePage1.classList.remove("hidden");
 });
 
-function count() {
-  counter++;
+// Page 1 button
+const page1Button = document.getElementById("page1-button");
+page1Button.addEventListener("click", function (){
+  gamePage1.classList.add("hidden");
+  gamePage2.classList.remove("hidden");
+});
 
-  if (counter % 10 === 0) {
-    alert("Mouse would like to thank you for the food 🥙");
-  }
+// Page 2 button
+const page2Button = document.getElementById("page2-button");
+page2Button.addEventListener("click", function (){
+  gamePage2.classList.add("hidden");
+  gamePage3.classList.remove("hidden");
+});
 
-  document.querySelector(".counter").innerHTML = counter;
-}
+// Page 3 button
+const page3Button = document.getElementById("page3-button");
+page3Button.addEventListener("click", function (){
+  gamePage3.classList.add("hidden");
+  startPage.classList.remove("hidden");
+});
+
+
+
+
